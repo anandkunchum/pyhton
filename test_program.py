000f41bfcbc59a8f4874ef43deeea58725df606f
anandkunchum <anand.kunchum@gmail.com>
2022-11-27 14:55:04 +0530

    updated assignment2 document

--- test_program.py
+++ test_program.py
@@ -1,4 +1,42 @@
 
-tuple = ('1','Hello')
-tuple = tuple + ('Anand',)
-print(tuple)
+set_a = {1,2,8,2}
+set_b = {"abc","xyz"}
+set_b.update(set_a)
+print(set_b)
+
+x = {"a", "b", "c"}
+y = {"c", "d", "e"}
+z = {"f", "g", "c"}
+result = x.intersection(y, z)
+print(result)
+
+var = {"name":"Anand","city":"chennai","company":"Nissan"}
+print(var.values())
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.get("model")
+print(x)
+fruits = ['apple', 'banana', 'cherry']
+fruits.pop(1)
+print(fruits)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.keys()
+print(x)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.values()
+print(x)
+
